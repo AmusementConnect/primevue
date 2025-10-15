3fd72a12d3872df40f064c86931e6d836f364876
ac-zmcnulty <zmcnulty@amusementconnect.com>
2025-10-15 10:34:48 -0500

    created acdateselector

--- packages/primevue/src/index.js
+++ packages/primevue/src/index.js
@@ -15,6 +15,12 @@ export * from 'primevue/usetoast';
 // UseStyle
 export * from 'primevue/usestyle';
 
+// ACDateSelector
+export * from 'primevue/acdateselector';
+export { default as ACDateSelector } from 'primevue/acdateselector';
+export * from 'primevue/acdateselector/style';
+export { default as ACDateSelectorStyle } from 'primevue/acdateselector/style';
+
 // Accordion
 export * from 'primevue/accordion';
 export { default as Accordion } from 'primevue/accordion';
@@ -812,3 +818,4 @@ export * from 'primevue/virtualscroller';
 export { default as VirtualScroller } from 'primevue/virtualscroller';
 export * from 'primevue/virtualscroller/style';
 export { default as VirtualScrollerStyle } from 'primevue/virtualscroller/style';
+
