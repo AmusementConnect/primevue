7bf1dafb77fb0a003f538e56df712ad52eb38715
ac-zmcnulty <zmcnulty@amusementconnect.com>
2025-10-15 10:34:08 -0500

    playground app

---
+++ apps/playground/src/main.js
@@ -0,0 +1,15 @@
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.mount('#app')
