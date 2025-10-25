1b750f6e0e5bcccafab473ea9ae7cf4a43f8bccb
ac-zmcnulty <zmcnulty@amusementconnect.com>
2025-10-25 14:41:14 -0500

    updates

--- packages/amusementconnect/src/index.js
+++ packages/amusementconnect/src/index.js
@@ -1,22 +1,3 @@
-/***************** PrimeVue (Auto-Generated) *****************/
-
-// Utils
-export * from 'primevue/utils';
-
-// UseConfirm
-export * from 'primevue/useconfirm';
-
-// UseDialog
-export * from 'primevue/usedialog';
-
-// UseToast
-export * from 'primevue/usetoast';
-
-// UseStyle
-export * from 'primevue/usestyle';
-
-// ACDateSelector
-export * from 'amusementconnect/acdateselector';
-export { default as ACDateSelector } from 'amusementconnect/acdateselector';
-export * from 'amusementconnect/acdateselector/style';
-export { default as ACDateSelectorStyle } from 'amusementconnect/acdateselector/style';
+// ACDateSelector Package
+export { default as ACDateSelector } from './acdateselector/ACDateSelector.vue';
+export { default as ACDateSelectorStyle } from './acdateselector/style/ACDateSelectorStyle.js';
